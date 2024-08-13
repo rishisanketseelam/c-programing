6173c021b94a496da1ec1f9d2ac874367986fa3c
rishisanketseelam <142652770+rishisanketseelam@users.noreply.github.com>
2024-08-13 21:58:04 +0530

    Update ascii.c

--- ascii.c
+++ ascii.c
@@ -5,6 +5,7 @@ void main (){
         while(ch,i<=127) {
 		if (ch<=127) {
                       printf(" ASCII character %d = \'%c\'\n",ch ,ch);
+		      printf(" ASCII character %d = \'%c\'\n",ch ,ch);
 	              ch= ch+1;
 	              i = i+1;
 		}
